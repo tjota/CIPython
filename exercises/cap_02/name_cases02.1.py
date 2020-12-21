63a7d23f461d1e525ef25824c1b39650e86d0cb5
Thiago Jack <thiagoojack@gmail.com>
2020-12-21 18:29:27 -0300

    Exercicios do capitulo 2

--- exercises/cap_02/name_cases02.1.py
+++ exercises/cap_02/name_cases02.1.py
@@ -1,3 +1,4 @@
+# Detalhes da citação de uma frase de um admirado autor.
 autor = "Mario Sérgio Cortella"
 frase = "Um erro deve ser corrigido, e não punido"
 mensagem = autor + ' certa vez disse: "' + frase + '"'
--- exercises/cap_02/name_cases03.py
+++ exercises/cap_02/name_cases03.py
@@ -1,6 +1,7 @@
 name = "\tThiago Jack de Oliveira\n  "
 
 print(name)
+# Removendo espaços desnecessários.
 print(name.rstrip())
 print(name.lstrip())
 print(name.strip())
